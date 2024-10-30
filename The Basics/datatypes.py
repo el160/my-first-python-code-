c2e628ad41f5f6a785da5e0b305df52b7cf55d31
el160 <ljhongeri04@gmail.com>
2024-10-30 12:19:53 +0300

    my first ever python code

---
+++ The Basics/datatypes.py
@@ -0,0 +1,40 @@
+#numeric data type
+number = 2
+name = "My name is Elijah Nyamweya"
+gender =  "Male"
+
+print(name,gender,number)
+
+#list data type
+fruits = ["apple", "banana", "cherry"]
+languages  = ["python", "java", "c++","python","dart"]
+print(fruits,languages)
+print(languages[2])
+print(languages[-3])
+
+
+#assigning multiple variables
+a,b,c = 5, 7, "Hello world"
+print(a) # prints 5
+print(b) # prints 7
+print(c) # prints Hello World
+
+
+#python is case sensitive
+num = 55
+Num = 510
+print(num) #5
+print(Num) #510
+
+#Tuple Data Type- same as list but once created cannot be changed
+products = ('XBox', 499.99, "Habibi", 23)
+print(products)
+
+#Set Data Type-unordered collection of unique items
+
+student_ids = {112, 114, 117, 113}
+print(student_ids)
+
+#Dictionary Data Type-orderd collection of items
+capital_city = {"Kenya": "Nairobi", "Nigeria": "Lagos"}
+print(capital_city)
---
+++ The Basics/index.py
@@ -0,0 +1,6 @@
+number = 2
+name = "My name is Elijah Nyamweya"
+gender =  "Male"
+
+print(name,gender,number)
+
